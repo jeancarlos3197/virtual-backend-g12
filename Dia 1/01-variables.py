f87c9bf00c20d0cbe177814372b9509692f35d42
jeancarlos3197 <carlos.ademir_66@hotmail.com>
2022-02-28 21:31:16 -0500

    declaracion de los primeros archivos

---
+++ Dia 1/01-variables.py
@@ -0,0 +1,21 @@
+# Esto es un comentario y sirve para dar contexto de que se hace, se hizo o se hara
+# TODO: logica para este controlador
+
+edad=25
+
+#Variables de texto
+nombre = 'jean'
+apellido = "G'arcia"
+
+#si queremos tener un texto que pueda contener saltos de lineas
+descripcion= '''Hola amigos:
+como estan?'''
+print(descripcion)
+
+#variables numericas
+year=2022
+
+#type mostrara el tipo  de variable
+print(type(year))
+
+#Python no se puede crear una variable sin contenido

54d0f251911cacd02bf100efbe9777dff6c98b0a
jeancarlos3197 <carlos.ademir_66@hotmail.com>
2022-03-01 16:01:50 -0500

    dia 1

--- Dia 1/01-variables.py
+++ Dia 1/01-variables.py
@@ -1,21 +1,55 @@
 # Esto es un comentario y sirve para dar contexto de que se hace, se hizo o se hara
 # TODO: logica para este controlador
 
-edad=25
+edad = 30
 
-#Variables de texto
-nombre = 'jean'
-apellido = "G'arcia"
+# variables de texto
+nombre = 'eduardo'
+apellido = "de'rivero"
 
-#si queremos tener un texto que pueda contener saltos de lineas
-descripcion= '''Hola amigos:
+# si queremos tener un texto que pueda contener saltos de linea
+descripcion = """hola amigos:
+como estan?
+yo muy bien jeje
+"""
+
+descripcion2 = '''¿hola amigos:
 como estan?'''
-print(descripcion)
 
-#variables numericas
-year=2022
+desCripcion2 = "adios"
 
-#type mostrara el tipo  de variable
+print(descripcion2)
+print(desCripcion2)
+
+# variables numericas
+year = 2022
+
+# type() => mostrara que tipo de variable es
 print(type(year))
+print(type(desCripcion2))
 
-#Python no se puede crear una variable sin contenido
+# Python no se puede crear una variable sin un contenido a exepcion del None
+# en python None = null | undefined
+especialidad = None
+
+# en Python no hace validacion del tipo de dato primario (si la variable 'nace' siendo string) normal se puede cambiar su tipo a otro (Boolean, int, float, array, etc.)
+# en Python no existen las constantes
+dni= [123123123]
+dni= 'peruano'
+dni= False
+
+# id() > dara la ubicacion de esa variablen en relacion a la memoria del dispositivo
+print(id(dni))
+
+
+print(type(especialidad))
+
+mes, dia = "febrero", 28
+
+print(mes)
+
+# del > elimina la variable de la memoria 
+del mes
+
+# si queremos usar luego de la eliminacion esa variable no sera posible ya que se elimino de la memoria
+print(mes)
--- Dia 1/02-impresion-pantalla.py
+++ Dia 1/02-impresion-pantalla.py
@@ -1,13 +1,13 @@
-nombre = 'jean'
+nombre = 'eduardo'
 
 print(nombre)
-#concatenar (juntar) varios valores
-print('El nombre es:', nombre,'del usuario')
+# concatenar (juntar) varios valores
+print('El nombre es:',nombre,'del usuario')
 
 estado_civil = 'viudo'
 
-# Si queremos usar el metodo format debemos coincidir el mismo numero de veces que colocamos {} con la cantidad de variables
+# si queremos usar el metodo format tenemos que coincidir el mismo numero de veces que colocamos el {} con la cantidad de variables
 print('La persona {} es {}'.format(nombre, estado_civil))
 
-# ademas podemos agregar la posición de la variable que  queremos imprimir dentro de las llaves
+# ademas podemos agregar la posicion de la variable que queremos imprimir dentro de las llaves
 print('{1} es una persona {0}'.format(estado_civil, nombre))

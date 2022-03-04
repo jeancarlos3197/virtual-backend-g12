7eb3d67fe4200fff2099fdf7031bf29904054b29
jeancarlos3197 <carlos.ademir_66@hotmail.com>
2022-03-03 21:37:59 -0500

    03 clases

--- Dia 4/02-explicaciones-excepciones.py
+++ Dia 4/02-explicaciones-excepciones.py
@@ -1,22 +1,26 @@
-productId=input('Ingresa el id del producto')
+productId = input('Ingresa el id del producto: ')
 
-# iremos a la db y budcaremos ese producto
+# iremos a la bd y buscaremos ese producto
 # 1, 2, 3, 4, 5, 6, 7, 8, 9
 try:
-  if(productId == '10'):
-    # emitira un error manualmente
-    # se utilizara para no continuar con el flujo normal del proyecto
-    raise Exception('El producto no existe en la bd')
-  # if validar la cantidad de productos
-  # if validar la fecha de vencimiento
-  # comenzare a crear mi metodo de pago
-  # if validar que el cliente tenga saldo suficiente
-  print('La pasarela de pagos concluyo exitosamente')
-  print('La ejecución continuara... ') #mercado pago
-
-except Exception as error:
-  print('Ups algo salio mal con el producto a buscar, mensaje: ', error.args[0])
+    if(productId == '10'):
+        # emitira un error manualmente 
+        # se utiliza para no continuar con el flujo normal del proyecto
+        raise Exception('El producto no existe en la bd')
+    # if validar la cantidad de productos
+    # if validar la fecha de vencimiento
+    # comenzare a crear mi metodo de pago 
+    # if validar que el cliente tenga saldo suficiente
+    print('La pasarela de pagos concluyo exitosamente...')
+    print('La ejecucion continuara ... ') # mercado pago 
+except Exception as e:
+    # ingresa si hubo error
+    print('Ups algo salio mal con el producto a buscar, mensaje:',e.args[0])
 else:
-  print('el producto encontrado es: ...')
+    # ingresa si no hubo error
+    print('El producto encontrado es: ...')
+finally:
+    # ingresa si hubo o no hubo error
+    print({'message':'Resultado final'})
 
 print('Yo soy el final del programa')

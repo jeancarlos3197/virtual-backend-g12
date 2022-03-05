31994fc363cd80b80851aef9cb754410b30e97df
jeancarlos3197 <carlos.ademir_66@hotmail.com>
2022-03-04 21:18:04 -0500

    avance dia 5

---
+++ Dia 5/01-librerias.py
@@ -0,0 +1,9 @@
+from camelcase import CamelCase
+
+instanciaCC = CamelCase()
+
+texto = 'Bienvenidos al mundo del backend'
+
+print(instanciaCC.hump(texto))
+
+# TODO: hacer lo mismo que el camelcase sin la librería

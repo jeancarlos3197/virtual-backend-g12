17c2b95de306998ecfbaa0a61eb6836f6f3c478a
jeancarlos3197 <carlos.ademir_66@hotmail.com>
2022-03-07 18:00:59 -0500

    ejercicios

--- Dia 5/ejercicios-clases.py
+++ Dia 5/ejercicios-clases.py
@@ -1,1 +1,46 @@
+from datetime import datetime
 # crear una clase Persona en la cual se guarden su nombre, fecha_nacimiento, nacionalidad, dni, ademas tambien una clase Alumno y una clase Docente en la cual el alumno , a diferencia del docente, tenga una serie de cursos matriculados, y el docente por su parte tenga un numero del seguro social y su cuenta de la CTS. En base a lo visto de herencia codificar las clases y ademas ver si hay algun atributo o metodo que deba de ser privado.
+class Persona:
+  def __init__(self, nombre, fecha_nacimiento, nacionalidad, dni):
+      self.nombre=nombre
+      self.fecha_nacimiento=fecha_nacimiento
+      self.nacionalidad=nacionalidad
+      self.dni=dni
+
+  def descripcion(self):
+    return 'Me llamo {}, naci el: {} , soy de {}, mi identificación es {}'.format(
+      self.nombre,
+      self.fecha_nacimiento,
+      self.nacionalidad,
+      self.dni
+      )
+
+class Alumno(Persona):
+  def __init__(self, nombre, fecha_nacimiento, nacionalidad, dni,cursos):
+      super().__init__(nombre, fecha_nacimiento, nacionalidad, dni)
+      self.cursos=cursos
+
+  def info(self):
+    return {
+      'Alumno': self.descripcion(),
+      'Cursos': self.cursos
+    }
+
+class Docente(Persona):
+  def __init__(self, nombre, fecha_nacimiento, nacionalidad, dni,seguro_social,cts):
+      super().__init__(nombre, fecha_nacimiento, nacionalidad, dni)
+      self.seguro_social=seguro_social
+      self.cts=cts
+
+  def info(self):
+    return{
+      'Docente': self.descripcion(),
+      'Seguro Social': self.seguro_social,
+      'CTS': self.cts
+    }
+
+alumno1 = Alumno('Jean',datetime.now().strftime('%Y-%m-%d %H:%M:%S'),'Peruano','54781245',['matematica','comunicacion'])   
+docente1 = Docente('carlos',datetime.now().strftime('%Y-%m-%d %H:%M:%S'),'Colombiano','54741451','ESSALUD','25431654885')
+
+print(alumno1.info())
+print(docente1.info())
--- Dia 5/ejercicios.py
+++ Dia 5/ejercicios.py
@@ -11,7 +11,15 @@ print('estos son los ejercicios')
 # ****
 # ****
 # dibujar_rectangulo()
+def dibujar_rectangulo():
+  altura=int(input('Ingrese la altura: '))
+  ancho=int(input('ingrese el ancho: '))
 
+  for i in range(altura):
+    for j in range(ancho):
+      print('*', end='')
+    print('')
+# dibujar_rectangulo()
 # Escribir una funcion que nosotros le ingresemos el grosor de un octagono y que lo dibuje
 # Ejemplo:
 # Grosor: 5
@@ -29,7 +37,10 @@ print('estos son los ejercicios')
 #      *******
 #       *****
 # dibujar_octagono()
+def dibujar_octagono(grosor):
+  pass
 
+dibujar_octagono(5)
 # Ingresar un numero entero y ese numero debe de llegar a 1 usando la serie de Collatz
 # si el numero es par, se divide entre dos
 # si el numero es impar, se multiplica por 3 y se suma 1
@@ -37,3 +48,15 @@ print('estos son los ejercicios')
 # Ejemplo 19
 # 19 58 29 88 44 22 11 34 17 52 26 13 40 20 10 5 16 8 4 2 1
 # serie_collatz()
+series=[]
+def serie_collatz(numero):
+  if numero !=1:
+    if numero%2==0:
+      numero = numero//2
+      series.append(numero)
+    else:
+      numero = (numero*3)+1
+      series.append(numero)
+    serie_collatz(numero)
+  return series
+# print(serie_collatz(50))

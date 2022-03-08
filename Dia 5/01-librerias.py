8d99b27b993f6519d74baa9ad38d0a8b58d65ddd
jeancarlos3197 <carlos.ademir_66@hotmail.com>
2022-03-07 20:50:42 -0500

    limpieza de la rama

--- Dia 5/01-librerias.py
+++
@@ -1,9 +0,0 @@
-from camelcase import CamelCase
-
-instanciaCC = CamelCase()
-
-texto = 'Bienvenidos al mundo del backend'
-
-print(instanciaCC.hump(texto))
-
-# TODO: hacer lo mismo que el camelcase sin la librería

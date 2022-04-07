b76fb54d4e97d26181cf04bb63db5cdf018acc73
jeancarlos3197 <carlos.ademir_66@hotmail.com>
2022-04-06 19:06:15 -0500

    modified views.py

--- agenda/gestion/views.py
+++ agenda/gestion/views.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.request import Request
+from rest_framework.generics import ListAPIView
 
 # https://www.django-rest-framework.org/api-guide/requests/
 @api_view(http_method_names=['GET', 'POST'])
@@ -15,3 +16,6 @@ def inicio(request: Request):
     return Response(data= {
       'message': 'Hiciste una peticion POST'
     }, status=201)
+
+class PruebaApiView(ListAPIView):
+  serializer_class = None
